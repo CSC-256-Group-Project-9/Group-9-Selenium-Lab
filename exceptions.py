e849c28f53f4d0c734251f409b9e0d773868fe9b
Robomonster <130997422+Robomonster@users.noreply.github.com>
2023-09-27 23:22:58 -0400

    Added Code Documentation

--- exceptions.py
+++ exceptions.py
@@ -1,16 +1,36 @@
 # Selenium Lab LLD 6.0
 
-class ElementNotFoundException:
-    pass
+class ElementNotFoundException(Exception):
+    """
+    (LLD 6.1)
+    """
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
 
 
-class NavigationException:
-    pass
+class NavigationException(Exception):
+    """
+    (LLD 6.1)
+    """
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
 
 
-class DataReadException:
-    pass
+class DataReadException(Exception):
+    """
+    (LLD 6.1)
+    """
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
 
 
-def handle_exception(exception_type, message):
+# (LLD 6.2) "Output: Customized error message/log"
+# pretty sure this means to use print or some other form of logging within the function rather than a return value  - William
+def handle_exception(exception_type: Exception, message: str) -> None:
+    """
+    (LLD 6.2) Handles specific exceptions, logging a user-friendly message.
+    Outputs Customized error message/log.
+    :param exception_type: Type of Exception
+    :param message: Message to log
+    """
     pass
